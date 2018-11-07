5961dd7448bea033e9e7ce6af82408e9a1096fa6
kit023 <superck38@gmail.com>
2018-11-07 09:53:30 +0800

    First Commit

--- events/views.py
+++ events/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplatelateView
+from django.views.generic import TemplatelateView, ListView, DetailView
 from .models import Event
 
 class HomePageView(TemplateView):
@@ -10,4 +10,9 @@ class HomePageView(TemplateView):
 class EventListVIew(LIstView):
     model = Event
     template_name = "event_list.html"
-    context_object_name = "event_list"
+    
+class EventDetailView(DetailView):
+    model = Event
+    template_name = "event_detail.html"
+    context_object_name = "event"
+    
